8edae7c3e80f32efd7e74fe7f85508c73cbef65b
omarbenites <omarbenites@github.com>
2017-01-04 13:40:54 -0500

    #update clase 4

---
+++ R_Clases/Clase4/rfunciones.R
@@ -0,0 +1,89 @@
+library(readxl)
+library(openxlsx)
+
+# Buscamos el archivo y capturamos la direccion
+fp <- file.choose()
+# Usamos el paquete readxl y examinamos las hojas o 'sheets' del archivo
+hojas <- excel_sheets(fp)
+
+# Vamos a leer la o sheet que deseamos
+cultivo <- "Tomate"
+datos <- readxl::read_excel(fp,sheet =  cultivo)
+
+# Creamos el nombre
+file <- paste(cultivo, ".xlsx", sep = "")
+
+
+wb <- createWorkbook()
+addWorksheet(wb, sheetName = cultivo)
+writeData(wb, sheet = cultivo, datos)
+saveWorkbook(wb, file = file, overwrite = TRUE)
+shell.exec(file)
+
+
+# Generando Funciones -----------------------------------------------------
+
+# Las funciones sirven para automatizar procesos. Se basan en el concepto de
+# recursividad, el cual busca simplificar procesos repetitivos en uno solo.
+
+# Estas constan de dos elementos:
+
+# - function name: el nombre que le vamos asignar a la funcion. Esta se genera
+# usando:  nombre_de_la_funcion <- function(arg1, arg2, arg3, ...)
+
+# - args o argumentos/parametros: son los inputs o elementos de entrada 
+# que requiere la funcion.
+
+# - outputs: las salidas que generan estas funciones.
+
+# Ejemplo 1
+
+# Identificar los argumentos o parametros de entrada que se repiten a lo largo
+# del script
+
+# Podemos identificar : 
+
+# A partir de este script vamos a crear una funcione
+
+
+
+cultivo <- "Tomate"
+datos <- readxl::read_excel(fp,sheet =  cultivo)
+
+# usuario <- "omar_pablo"
+file <- paste(cultivo, ".xlsx", sep = "")
+
+wb <- createWorkbook()
+addWorksheet(wb, sheetName = cultivo)
+writeData(wb, sheet = cultivo, datos)
+saveWorkbook(wb, file = file, overwrite = TRUE)
+
+
+
+
+
+
+
+
+# dt_transf <- function(fp, cultivo){
+#   
+#   #cultivo <- "Tomate"
+#   datos <- readxl::read_excel(fp, sheet =  cultivo)
+#   
+#   #usuario <- "omar_pablo"
+#   file <- paste(cultivo, ".xlsx", sep = "")
+#   
+#   wb <- createWorkbook()
+#   addWorksheet(wb, sheetName = cultivo)
+#   writeData(wb, sheet = cultivo, datos)
+#   saveWorkbook(wb, file = file, overwrite = TRUE)
+#   
+# }
+
+  
+  
+
+
+
+
+
