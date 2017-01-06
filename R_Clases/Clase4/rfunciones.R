c471fcc9e768164e5b20c4a27d3753440c598c35
omarbenites <omarbenites@github.com>
2017-01-05 19:55:58 -0500

    #update in rfunciones

--- R_Clases/Clase4/rfunciones.R
+++ R_Clases/Clase4/rfunciones.R
@@ -1,6 +1,8 @@
 library(readxl)
 library(openxlsx)
 
+install.packages(c("readxl","openxlsx"))
+
 # Buscamos el archivo y capturamos la direccion
 fp <- file.choose()
 # Usamos el paquete readxl y examinamos las hojas o 'sheets' del archivo
@@ -8,16 +10,23 @@ hojas <- excel_sheets(fp)
 
 # Vamos a leer la o sheet que deseamos
 cultivo <- "Tomate"
-datos <- readxl::read_excel(fp,sheet =  cultivo)
+datos <- read_excel(fp , sheet =  cultivo)
 
 # Creamos el nombre
 file <- paste(cultivo, ".xlsx", sep = "")
 
-
+# Crea en memoria el archivo
 wb <- createWorkbook()
+
+# Agregamos un sheet o hoja al archivo
 addWorksheet(wb, sheetName = cultivo)
+
+# Escribir los datos de la hoja "Tomate" en un nuevo archivo
 writeData(wb, sheet = cultivo, datos)
+
+#salvamos los datos
 saveWorkbook(wb, file = file, overwrite = TRUE)
+
 shell.exec(file)
 
 
@@ -45,21 +54,33 @@ shell.exec(file)
 
 # A partir de este script vamos a crear una funcione
 
+# Input1: nombre o ruta del archivo
+# Input2: Nombre del cultivo (cultivo)
 
+dt_tranf <- function(fp, cultivo) {
 
-cultivo <- "Tomate"
+#cultivo <- "Tomate"
 datos <- readxl::read_excel(fp,sheet =  cultivo)
 
-# usuario <- "omar_pablo"
 file <- paste(cultivo, ".xlsx", sep = "")
 
 wb <- createWorkbook()
 addWorksheet(wb, sheetName = cultivo)
 writeData(wb, sheet = cultivo, datos)
 saveWorkbook(wb, file = file, overwrite = TRUE)
+shell.exec(file = file)
+
+}
+
+dt_tranf(fp = fp, cultivo = "Tomate")
+dt_tranf(fp = fp, cultivo = "Arroz")
+dt_tranf(fp = fp, cultivo = "yuca")
 
 
+# Observacion:
 
+# En el caso de tener solamente 3 tablas, eliminar la primera.
+# Solo quedarse con las 2 ultimas.
 
 
 
