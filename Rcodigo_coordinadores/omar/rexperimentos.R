9528ef1bef70a76c7b35259d42d4b3d6196ee19e
omarbenites <omarbenites@github.com>
2017-01-30 16:51:31 -0500

    #REXPERIMENTO

--- Rcodigo_coordinadores/omar/rexperimentos.R
+++ Rcodigo_coordinadores/omar/rexperimentos.R
@@ -8,7 +8,7 @@ library(openxlsx)
 
 # 5 treatments and 6 blocks
 trt<-c("canchan","tomasa","amarilla","victoria","revolucion99")
-design <-design.rcbd(trt , 4, serie=2) # seed = 986
+design <-design.crd(trt , 4, serie=2) # seed = 986
 design
 book <- design$book # field book o libro de campo
 write.xlsx(book, "dca.xlsx")
@@ -23,8 +23,34 @@ trt<-c("canchan","tomasa","amarilla","victoria","revolucion99")
 design <-design.rcbd(trt , 4, serie=2) # seed = 986
 design
 book <- design$book # field book o libro de campo
-write.xlsx(book, "dca.xlsx")
-shell.exec("dca.xlsx")
+write.xlsx(book, "dbca.xlsx")
+shell.exec("dbca.xlsx")
+
+
+# Diseno Factorial
+
+library(agricolae)
+# 3 fertilizantes a 2 difernetes dosis
+trt<-c(3,2) # factorial 3x2
+outdesign <-design.ab(trt, r=3, serie=2)
+
+#hacking
+f <- c("F1","F2","F3")
+f <- data.frame()
+
+
+d <- c("d0","d100")
+book<-outdesign$book
+
+library(dplyr)
+book %>% left_join(, )
+
+
+
+
+
+# Diseno Cuadrado Latino
+
 
 
 
